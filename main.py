a613de4f76350ba8974f45896ee9a7144a2e3c46
unknown <edwin.wagha20@gmail.com>
2023-09-09 17:33:51 +0300

    Starting UI

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 ### Weather Application Programming Interface ###
 
+import tkinter as tk
+from tkinter import ttk
+
+class MyApp(object):
+	def __init__(self, arg):
+		self.root = root
+		self.root.title("Weather API")
+
+		# set a fixed window size 
+		self.root.geometry("400x200")
+
+		
+
+if __name__ == '__main__':
+	root = tk.Tk()
+	app = MyApp(root)
+	root.mainloop()
