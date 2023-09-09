a4e04be5d77c91ecf7a354567a2e7db54f4aa9d2
Edwin Wagha <edwin.wagha20@gmail.com>
2023-09-09 18:43:04 +0300

    Entry Widget Update

--- main.py
+++ main.py
@@ -11,7 +11,20 @@ class MyApp(object):
 		# set a fixed window size 
 		self.root.geometry("400x200")
 
-		
+		# create frames for the widgets
+		self.frame01 = ttk.Frame(root)
+		self.frame01.grid(row=0, column=0, padx=20, pady=10)
+
+		self.frame02 = ttk.Frame(root)
+		self.frame02.grid(row=1, column=0)
+
+		self.frame03 = ttk.Frame(root)
+		self.frame03.grid(row=2, column=0)
+
+		# create an entry for the location input
+		self.entry_location = ttk.Entry(self.frame01)
+		self.entry_location.grid(row=0, column=0, padx=(0, 20))
+
 
 if __name__ == '__main__':
 	root = tk.Tk()
