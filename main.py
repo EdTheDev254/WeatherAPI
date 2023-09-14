3f6424770fab05fa6e0cb3b9103f4ac9b86bf74f
Edwin Wagha <edwin.wagha20@gmail.com>
2023-09-14 13:55:05 +0300

    GUIFullUpdate

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ class MyApp(object):
 		self.root.maxsize(500, 250)
 
 		# Create a style object
-		style = ttk.Style()
+		#style = ttk.Style()
 		# Set the font for the ttk buttons
-		style.configure("TButton", font=("Arial", 10))  # Change "Arial" and 10 to your desired font family and size
+		#style.configure("TButton", font=("Arial", 10))  # Change "Arial" and 10 to your desired font family and size
 
 		# set a fixed window size 
 		self.root.geometry("500x250")
@@ -26,9 +26,9 @@ class MyApp(object):
 		self.frame03 = tk.Frame(self.root)
 
 		# grid the frames
-		self.frame01.grid(row=0, column=0, padx=(20, 10), pady=20, sticky='n')
+		self.frame01.grid(row=0, column=0, padx=(20, 20), pady=20, sticky='n')
 		self.frame02.grid(row=0, column=1, padx=(0, 20), pady=20, sticky='n')
-		self.frame03.grid(row=1, column=0, padx=20, sticky='w', columnspan=2)
+		self.frame03.grid(row=1, column=0, padx=(20, 0),sticky='w', columnspan=2)
 
 		# label text
 		self.label_text = tk.Label(self.frame01, text='Enter location name')
@@ -51,12 +51,12 @@ class MyApp(object):
 		self.output_field.grid(row=0, column=0)
   
 		# output data button
-		self.output_data_button = ttk.Button(self.frame03, text='Output Data')
+		self.output_data_button = ttk.Button(self.frame03, text='Output Data', padding=10, width=25)
 		self.output_data_button.grid(row=0, column=0)
   
   		# reset button
-		self.reset_button = ttk.Button(self.frame03, text='')
-		self.reset_button.grid(row=0, column=1)
+		self.reset_button = ttk.Button(self.frame03, text='Reset', padding=10, width=37)
+		self.reset_button.grid(row=0, column=1, padx=(30, 0))
   
 		
   
